929e76e4ca039b36f6afd41a451550d5f2d7458d
unknown <kuzeykanl@gmail.com>
2026-05-11 13:50:52 +0300

    update food items and may monthly menu

--- src/scripts/uploadMonthlyMenu.ts
+++ src/scripts/uploadMonthlyMenu.ts
@@ -5,22 +5,25 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const MONTHLY_MENU = [
-  { date: "2026-05-01", items: "tavuk, pilav, mercimek, baklava" },
-  { date: "2026-05-02", items: "balık, bezelye, ezogelin, sütlaç" },
-  { date: "2026-05-03", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-04", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-05", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-06", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-07", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-08", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-09", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-10", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-11", items: "ezogelin, çoban, pilav, baklava, ayran" },
-  { date: "2026-05-12", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-13", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-14", items: "köfte, makarna, mercimek, kazandibi" },
-  { date: "2026-05-15", items: "köfte, makarna, mercimek, kazandibi" },
-  // add all days here
+  { date: "2026-05-01", items: "şehriyeli tavuksuyu çorba, etli maraş taze fasulye, çıtır kadayıflı kup, sebzeli bulgur pilavı" },
+  { date: "2026-05-04", items: "ezogelin, döner, pirinç pilavı, meşrubat" },
+  { date: "2026-05-05", items: "kuru fasulye, yayla, arpa şehriyeli pilav, mevsim salata" },
+  { date: "2026-05-06", items: "karışık ızgara, anadolu, peynirli makarna, mevsim meyve" },
+  { date: "2026-05-07", items: "mercimek, mantı, şakşuka, yoğurt, kadayıf" },
+  { date: "2026-05-08", items: "köz domates, susamlı tavuk, tepsi börek, ayran" },
+  { date: "2026-05-11", items: "şehriyeli bulgur, yoğurt çorbası, haşlama, mevsim salata" },
+  { date: "2026-05-12", items: "mercimek, dolma, su böreği, yoğurt, baklava" },
+  { date: "2026-05-13", items: "soslu makarna, ezogelin, inegöl, mevsim meyve" },
+  { date: "2026-05-14", items: "tel şehriyeli pilav, köylü, karnıyarık, cacık" },
+  { date: "2026-05-15", items: "arpa şehriye çorba, çökertme, pilav, yoğurt, komposto" },
+  { date: "2026-05-18", items: "tel şehriyeli pilav, ezogelin, filiz kebap, sütlaç" },
+  { date: "2026-05-19", items: "milföy, mercimek, ıspanak, yoğurt, meşrubat" },
+  { date: "2026-05-20", items: "çıtır tavuk, soslu makarna, tarhana, ayran" },
+  { date: "2026-05-21", items: "mantar, barbunya, sandal sefası, arpa şehriyeli pilav, kek, mevsim meyve" },
+  { date: "2026-05-22", items: "şehriyeli bulgur, mahluta, soslu köfte, cacık" },
+  { date: "2026-05-25", items: "yayla, türlü, arpa şehriyeli pilav, irmik" },
+  { date: "2026-05-26", items: "ezogelin, orman kebabı, sebzeli bulgur, cacık" },
+  { date: "2026-05-31", items: "kanat, soslu makarna, mercimek, ayran" },
 ];
 
 async function uploadMonthlyMenu() {
